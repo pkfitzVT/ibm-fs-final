70b510e845bcf665676c51c19f911e36eb6a7694
pkfitzVT <pkeenanfitzgerald@gmail.com>
2025-05-31 07:33:17 -0400

    feature/general user get all books added to general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -22,10 +22,22 @@ public_users.get("/", function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get("/isbn/:isbn", function (req, res) {
-  // …to be implemented later…
-  return res.status(300).json({ message: "Yet to be implemented" });
+public_users.get("/isbn/:isbn", (req, res) => {
+  // 1. Read the ISBN out of req.params
+  const isbn = req.params.isbn;
+
+  // 2. Look up that key in the `books` object
+  if (books[isbn]) {
+    // 3a. If it exists, return HTTP 200 with the book’s JSON
+    return res.status(200).json(books[isbn]);
+  } else {
+    // 3b. If not, return 404 “not found”
+    return res
+        .status(404)
+        .json({ message: `Book with ISBN ${isbn} not found` });
+  }
 });
+
 
 // Get book details based on author
 public_users.get("/author/:author", function (req, res) {
